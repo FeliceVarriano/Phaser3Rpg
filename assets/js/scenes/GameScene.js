b8bd9e3fc95d77b0e01ae3d405316ffb9cf3d755
Felice Varriano <felice.varriano@gmail.com>
2020-06-07 18:53:03 -0400

    Player, chest classes, boot, title, game and ui scene skeletons. Boot and Title scenes completed.

---
+++ assets/js/scenes/GameScene.js
@@ -0,0 +1,37 @@
+class GameScene extends Phaser.Scene{
+    constructor(){
+        super('Game');
+    }
+
+    create(){
+        var goldPickupAudio = this.sound.add('goldSound', {loop: false});
+    
+        var button = this.add.image(100, 100, 'button1');
+        button.setOrigin(0.5,0.5);
+    
+        this.add.sprite(300, 100, 'button1');
+    
+        this.chest = new Chest(this, 300, 300, 'items', 0);
+    
+    
+        this.wall = this.physics.add.image(500, 100, 'button1');
+        this.wall.setImmovable();
+    
+    
+        this.player = new Player(this, 32, 32, 'characters', 0);
+        
+    
+        this.physics.add.collider(this.player, this.wall);
+        this.physics.add.overlap(this.player, this.chest, function(player, chest){
+            goldPickupAudio.play();
+            chest.destroy();
+        });
+    
+        this.cursors = this.input.keyboard.createCursorKeys();
+    
+    }
+
+    update(){
+        this.player.update(this.cursors);
+    }
+}
